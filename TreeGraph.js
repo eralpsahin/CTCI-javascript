90242145b694b9e8fbb406bb4ff2b27c16ed567b
Eralp Sahin <eralpsahin@me.com>
2019-08-08 19:24:24 -0400

    Add Questions 4.2 - 4.7

--- TreeGraph.js
+++ TreeGraph.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const print = console.log;
 
 // * TREES AND GRAPHS CHAPTER
@@ -117,7 +116,8 @@ GraphNode.prototype.setRight = function(val) {
         print('----');
         queue.push(null);
         if (queue[0] === null) break;
-        // You are encountering two consecutive `nulls` means, you visited all the nodes.
+        // You are encountering two consecutive null means
+        // you visited all the nodes.
         else continue;
       }
       // Visit the current
@@ -213,16 +213,15 @@ GraphNode.prototype.setRight = function(val) {
  * insert(), size, clear(), find(), getHeight()
  * getMin(), getMax(), isBinary(), remove()
  */
+const BSTNode = function(val, left = null, right = null) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+};
 (function() {
   const BST = function() {
     this.root = null;
     this.size = 0;
-  };
-
-  const BSTNode = function(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
   };
 
   BSTNode.prototype.insert = function(val) {
@@ -263,10 +262,7 @@ GraphNode.prototype.setRight = function(val) {
     return this.val;
   };
 
-  BSTNode.prototype.successor = function() {
-    if (!this.right) return null;
-    return this.right.getMin();
-  };
+  // * successor is Question 4.6
 
   BST.prototype.insert = function(val) {
     this.size += 1;
@@ -308,8 +304,8 @@ GraphNode.prototype.setRight = function(val) {
   };
 
   // TODO: remove()
-  // TODO: isBinary()
-});
+  // * isBinary is Question 4.5
+})();
 
 /**
  * * Binary Search on sorted Array
@@ -332,5 +328,203 @@ GraphNode.prototype.setRight = function(val) {
 })();
 
 /**
- * ! Binary Search Tree Definition
+ * ! BinarySearch on sorted Array
+ */
+
+/**
+ * * Question 4.2 Minimal Tree
+ */
+
+(function() {
+  const nums = [1, 3, 5, 8, 9, 11, 14, 16, 18, 19, 20];
+  const root = new BSTNode(nums[Math.floor(nums.length / 2)]);
+  const createBST = function(root, nums) {
+    if (nums.length <= 1) return;
+    let middle = Math.floor(nums.length / 2);
+    let left = Math.floor(middle / 2);
+
+    let right = Math.floor(middle / 2) + Math.ceil(nums.length / 2);
+    if (left === right) right++;
+    print(left, right, nums);
+    root.left = new BSTNode(nums[left]);
+    if (nums.length !== 2) root.right = new BSTNode(nums[right]);
+    createBST(root.left, nums.slice(0, middle));
+    createBST(root.right, nums.slice(middle + 1));
+  };
+  createBST(root, nums);
+});
+
+/**
+ * ! Question 4.2
+ */
+
+/**
+ * * Question 4.3 List of Depths
+ * This can easily be converted to a linkedList
+ */
+
+(function() {
+  const depths = [];
+
+  const createDepthList = function(root, depth) {
+    if (!root) return;
+    if (depths.length === depth) depths.push([root.val]);
+    else depths[depth].push(root.val);
+    createDepthList(root.left(), depth + 1);
+    createDepthList(root.right(), depth + 1);
+  };
+});
+
+/**
+ * ! Question 4.3
+ */
+
+/**
+ * * Question 4.4 Check Balanced
+ */
+(function() {
+  let res = true;
+  BSTNode.prototype.checkBalanced = function() {
+    let leftHeight, rightHeight;
+    if (this.left) leftHeight = this.left.getHeight();
+    else leftHeight = 0;
+    if (this.right) rightHeight = this.right.getHeight();
+    else rightHeight = 0;
+    if (Math.abs(leftHeight - rightHeight) > 1) res = false;
+    let height = Math.max(leftHeight, rightHeight);
+    return height + 1;
+  };
+});
+/**
+ * ! Question 4.4
+ */
+
+/**
+ * * Question 4.5 Validate BST
+ * We can store max of left sub-tree and min of right sub-tree
+ * and check if values hold the BST property for the parent
+ */
+
+(function() {
+  let res = true;
+  BSTNode.prototype.getMinMax = function() {
+    // A leaf node
+    if (!this.left && !this.right) return [this.val, this.val];
+    let left, right;
+    let min = this.val,
+      max = this.val;
+    // Get the maximum of left side
+    if (this.left) {
+      left = this.left.getMinMax();
+      // max from left can't be greater than this.val
+      if (this.val < left[1]) {
+        res = false;
+        return [null, null];
+      }
+      max = left[1];
+    }
+
+    // Get the minimum of right side
+    if (this.right) {
+      right = this.right.getMinMax();
+      // min from right can't be less than this.val
+      if (this.val > right[0]) {
+        res = false;
+        return [null, null];
+      }
+      min = right[0];
+    }
+    print(this.val, left, right);
+    return [max, min];
+  };
+});
+
+/**
+ * ! Question 4.5
+ */
+
+/**
+ * * Question 4.6 Successor
+ * BSTNode needs to have a parent pointer as well.
+ */
+
+(function() {
+  const BSTParent = function(val, parent = null) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.parent = parent;
+  };
+  BSTParent.prototype.getMin = BSTNode.prototype.getMin;
+
+  BSTParent.prototype.successor = function() {
+    if (!this.right) return this.getLeftParent();
+    return this.right.getMin();
+  };
+
+  BSTParent.prototype.getLeftParent = function() {
+    if (!this.parent) return null;
+    if (this.parent.left === this) return this.parent.val;
+    else {
+      return this.parent.getLeftParent();
+    }
+  };
+});
+/**
+ * ! Question 4.6
+ */
+
+/**
+ * * Question 4.7 Build Order
+ * Simple Topological Sort on Dependency Adjacency Matrix in this case..
+ * projects is an array, dependencies is an array of arrays(size 2)
+ */
+(function() {
+  const createHash = function(projects, dependencies) {
+    // Create adjacency matrix
+    const depends = {};
+    for (let i = 0; i < dependencies.length; i += 1) {
+      let dependent = dependencies[i][1];
+      let dependency = dependencies[i][0];
+      if (depends[dependent] === undefined) {
+        depends[dependent] = {
+          [dependency]: true
+        };
+      } else {
+        depends[dependent][dependency] = true;
+      }
+    }
+    return depends;
+  };
+
+  const dependencySort = function(projects, dependencies) {
+    const depends = createHash(projects, dependencies);
+    let sorted = [];
+    let nondependent = [];
+    print(depends);
+    // Fill nondependent array at the beginning
+    for (let i = 0; i < projects.length; i += 1) {
+      if (!depends[projects[i]]) nondependent.push(projects[i]);
+    }
+
+    // Start Topological Sort
+    while (nondependent.length > 0) {
+      let node = nondependent.shift();
+      sorted.push(node);
+      for (let dependent in depends) {
+        if (depends[dependent][node]) {
+          delete depends[dependent][node];
+          if (Object.keys(depends[dependent]).length === 0) {
+            nondependent.push(dependent);
+            delete depends[dependent];
+          }
+        }
+      }
+    }
+    return sorted;
+  };
+});
+
+/**
+ * ! Question 4.7
  */
